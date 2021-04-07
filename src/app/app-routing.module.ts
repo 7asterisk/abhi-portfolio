25e5613481caaa2da237bbbe744a99b670293c8b
Abhishek kumar <desingboad@gmail.com>
2021-04-07 21:29:24 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { SkillsComponent } from './skills/skills.component';
+import { ExperienceComponent } from './experience/experience.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: 'about',pathMatch: 'full' },
+  { path: 'about', component: HomeComponent },
+  { path: 'skills', component: SkillsComponent },
+  { path: 'projects', component: ExperienceComponent }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
